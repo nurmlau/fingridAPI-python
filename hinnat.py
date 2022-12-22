2f7c8298885b29777e996edd656a3ab90897b983
Lauri Nurmela <lauri.nurmela@gmail.com>
2022-12-22 17:24:40 +0200

    0.9

--- hinnat.py
+++ hinnat.py
@@ -1,7 +1,5 @@
 import requests
-import xml.etree.ElementTree as ET
 import xmltodict
-import json
 from datetime import date
 import datetime
 import secrets
@@ -21,24 +19,27 @@ result = requests.get(url, headers = {})
 
 data = xmltodict.parse(result.content)
 
-
-
 hours = ["00-01", "01-02", "02-03", "03-04", "04-05", "05-06", "06-07", "07-08", "08-09", "09-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16", "16-17", "17-18", "18-19", "19-20", "20-21", "21-22", "22-23", "23-24",]
 
-temp = []
-for item in data["Publication_MarketDocument"]["TimeSeries"]:
+try:
 
-    for i in item["Period"]["Point"]:
-        price = float(i["price.amount"]) / mw * 100
-        temp.append(price)
+    temp = []
+    for item in data["Publication_MarketDocument"]["TimeSeries"]:
 
-priceData = []
-for i in range(47):
-    if i >= 23:
-        priceData.append(temp[i])
+        for i in item["Period"]["Point"]:
+            price = float(i["price.amount"]) / mw * 100
+            temp.append(price)
 
-for i in range(24):
-    print(tomorrow, hours[i], "{:.2f}".format(priceData[i]))
+    priceData = []
+    for i in range(47):
+        if i >= 23:
+            priceData.append(temp[i])
+
+    for i in range(24):
+        print(tomorrow, hours[i], "{:.2f}".format(priceData[i]))
+
+except TypeError:
+    print("Data not available yet")
 
     
 
